c6769ffbfedc8dfaadb645a03f075633bbb87197
Tamboli <tambolisadik16@gmail.com>
2021-05-12 12:21:35 +0530

    updated

--- python.py
+++ python.py
@@ -1,9 +1,11 @@
 #This is simple python code
 class python:
-    def __init__(self,a,b):
+    def __init__(self,a,b,c):
         self.a=a
         self.b=b
+        self.c=c
         print("First no:",self.a)
         print("Second no:",self.b)
+        print("Third no:",self.c)
 
-py=python(3,4)
+py=python(3,4,5)
