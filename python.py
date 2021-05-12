9efb185cce55063214f1ad13ed2f97b6c725b760
Tamboli <tambolisadik16@gmail.com>
2021-05-12 12:16:17 +0530

    Python code

---
+++ python.py
@@ -0,0 +1,9 @@
+#This is simple python code
+class python:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+        print("First no:",self.a)
+        print("Second no:",self.b)
+
+py=python(3,4)
